790f749048114f8dcd92d4c2ebb39e6b0ef0b280
Renato Rosa <re.rosa98gmail.com>
2020-10-14 19:27:54 -0300

    mostrando imagens em index e show, começando views

--- server/src/controllers/OrphanagesController.ts
+++ server/src/controllers/OrphanagesController.ts
@@ -7,7 +7,9 @@ export default {
     //Lista todos orfanatos
     async index(req: Request, res: Response) {
         const orphanagesRepository = getRepository(Orphanage)
-        const orphanages = await orphanagesRepository.find()
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        })
         return res.json(orphanages)
     },
 
@@ -15,7 +17,9 @@ export default {
     async show(req: Request, res: Response) {
         const { id } = req.params
         const orphanagesRepository = getRepository(Orphanage)
-        const orphanage = await orphanagesRepository.findOneOrFail(id)
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        })
         return res.json(orphanage)
     },
 
